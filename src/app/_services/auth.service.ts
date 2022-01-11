161fcadeaa8f08903d27749502937bfaee771809
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-11 12:18:05 +0100

    UseCase2 complete

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -23,10 +23,6 @@ export class AuthService {
   async createUserWithEmailAndPassword (user: User, redirectToURL?: string) {
     return this.afAuth.createUserWithEmailAndPassword(user.email, user.password)
     .then(async (result) => {
-      const afuser = await this.afAuth.user.toPromise();
-      afuser.displayName = user.displayname;
-
-      this.afAuth.updateCurrentUser(afuser);
 
       window.alert('You have been successfully registered!');
       this.router.navigate(['/' + redirectToURL]);
